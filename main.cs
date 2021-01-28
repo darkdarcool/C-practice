9102f30a3161fc99e0c5cd3c8a1ebd58c652cf88
DELETED ACCOUNT 100% <66882633+darkdarcool@users.noreply.github.com>
2021-01-28 11:08:06 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -19,9 +19,15 @@ class MainClass {
     Console.ForegroundColor = ConsoleColor.Blue;
     sp("Hello " + name + "!", options[choice]);
 		Thread.Sleep(2000);
-		sp("This is a text adventure that will be compiled in the github GCC and c sharp. This may take a long to time to load so when a prompt that says loading comes up please do not dismiss it.")
-	  
-		
+		sp("This is a text adventure that will be compiled in the github GCC and c sharp. This may take a long to time to load so when a prompt that says loading comes up please do not dismiss it.", options[choice]);
+		Thread.Sleep(2000);
+		Console.Clear();
+		Console.ResetColor();
+		Console.ForegroundColor = ConsoleColor.Green;
+		sp("Feeding into compiler...", options[choice]);
+		Console.ResetColor();
+		Console.ForegroundColor = ConsoleColor.Blue;
+		sp("Put text here.", options[choice]);
 	  
   }
 
